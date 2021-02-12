ba66548e26004e1e6723eed9f5370d501942d844
maxim darre <maxim.darre@gmail.com>
2021-02-12 16:25:11 -0500

    feat(backend-starter): add backen-starter application

---
+++ apps/backend-starter/src/app.module.ts
@@ -0,0 +1,17 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ apps/backend-starter/src/user/graphql/dtos/user-find-many.dto.ts
@@ -0,0 +1,28 @@
+import { ArgsType, Field, ID } from '@nestjs/graphql';
+import { Type } from 'class-transformer';
+import { IsBoolean, IsInt, IsOptional, IsString } from 'class-validator';
+
+@ArgsType()
+export class UserReadManyDto {
+  @Field(() => ID, { nullable: true })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  id?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  banned?: boolean;
+}
