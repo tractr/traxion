3e620b566b88b28c153b40a27daf13c675ddd87b
maxim darre <maxim.darre@gmail.com>
2021-02-18 17:29:09 -0500

    refactor(backend starter): clean backend starter

--- apps/backend-starter/src/user/common/decorators/public.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata, CustomDecorator } from '@nestjs/common';
-
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = (): CustomDecorator<string> =>
-  SetMetadata(IS_PUBLIC_KEY, true);
