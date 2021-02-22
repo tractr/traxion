a1d998fe11f107fa8e6fb72fcf46eaded255659c
maxim darre <maxim.darre@gmail.com>
2021-02-22 16:50:19 -0500

    feat(model templates): add enums to prisma schema and dto validation

--- apps/test-backend-starter/src/example/user/common/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './services';
-export * from './user-model.module';
-export * from './user-model.constants';
--- apps/test-backend-starter/src/example/user/common/user-model.module.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { UserDatabaseService, UserService } from './services';
-
-export const UserModelModuleProvidersList = [UserService, UserDatabaseService];
-export const UserModelModuleExportsList = [UserService, UserDatabaseService];
