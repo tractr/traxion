44c1ea443079628d31ab267a944480be41e621fb
floross <floross@users.noreply.github.com>
2021-02-25 16:27:57 -0500

    fix: add prisma generation into version control and start to configure nestjs-user to export files

--- packages/hapify-plugin-nestjs-user/src/authentication/controllers/login.controller.ts
+++ packages/hapify-plugin-nestjs-user/src/authentication/controllers/login.controller.ts
@@ -7,7 +7,7 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { User } from '../../generated/prisma';
 import { Request, Response } from 'express';
 
 import { AUTHENTICATION_MODULE_OPTIONS } from '../constants';
--- packages/hapify-plugin-nestjs-user/src/generated.example/user/common/index.ts
+++ packages/hapify-plugin-nestjs-user/src/generated.example/user/common/index.ts
@@ -1,3 +1,2 @@
 export * from './services';
 export * from './user-model.module';
-export * from './user-model.constants';
