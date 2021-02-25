63153f82aba323e5f6195514d2c0a16528d95022
floross <floross@users.noreply.github.com>
2021-02-25 18:31:34 -0500

    feat(nestjs-user): link nestjs-user npm packages to test-backend-starter for the first time

--- apps/test-backend-starter/src/app.module.ts
+++ apps/test-backend-starter/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from '@tractr/hapify-plugin-nestjs-core';
+import { DatabaseModule } from '@tractr/hapify-plugin-nestjs-database';
+import { AuthenticationModule } from '@tractr/hapify-plugin-nestjs-user';
+import { UserService } from '@tractr/hapify-plugin-nestjs-user/lib/src/generated/user';
+
 import { AppController } from './controllers';
 import { ModelsModule } from './generated';
+import { UserCustomService } from './services';
 
 @Module({
-  imports: [CoreModule, ModelsModule.register()],
+  imports: [
+    CoreModule,
+    DatabaseModule,
+    ModelsModule.register(),
+    AuthenticationModule.register(undefined, {
+      providers: [{ provide: UserService, useClass: UserCustomService }],
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
