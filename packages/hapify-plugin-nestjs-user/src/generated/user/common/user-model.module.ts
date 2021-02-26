c992d4febffc02cb19084d2484fda293bdc9e369
floross <floross@users.noreply.github.com>
2021-02-26 09:06:11 -0500

    fix(nestjs-user): use named DI to be able to override inside the modules

--- packages/hapify-plugin-nestjs-user/src/generated/user/common/user-model.module.ts
+++ packages/hapify-plugin-nestjs-user/src/generated/user/common/user-model.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ModuleOverride } from '@tractr/hapify-plugin-nestjs-core';
-import { DatabaseModule } from '@tractr/hapify-plugin-nestjs-database';
 import { UserDatabaseService, UserService } from './services';
-
+import { USER_DATABASE_SERVICE, USER_SERVICE } from './user-model.constant';
+const providers = [
+  {
+    provide: USER_SERVICE,
+    useClass: UserService
+  },
+  {
+    provide: USER_DATABASE_SERVICE,
+    useClass: UserDatabaseService],
+  },
+];
 @Module({
-  imports: [DatabaseModule],
-  exports: [UserService, UserDatabaseService],
-  providers: [UserService, UserDatabaseService],
+  exports: [providers],
+  providers: [providers],
 })
 export class UserModelModule extends ModuleOverride {}
--- packages/hapify-plugin-nestjs-user/src/generated/user/rest/user-rest.module.ts
+++ packages/hapify-plugin-nestjs-user/src/generated/user/rest/user-rest.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ModuleOverride } from '@tractr/hapify-plugin-nestjs-core';
-import { DatabaseModule } from '@tractr/hapify-plugin-nestjs-database';
 import { UserModelModule } from '../common';
 import { UserController } from './controllers';
 import { UserRestDtoService } from './services';
-
+import { USER_CONTROLLER, USER_REST_DTO_SERVICE } from './ user-rest.constant';
+const providers = [
+  { provide: USER_REST_DTO_SERVICE, useClass: UserRestDtoService }
+];
 @Module({
-  imports: [DatabaseModule],
-  exports: [UserRestDtoService],
-  providers: [UserRestDtoService],
-  controllers: [UserController],
+  exports: providers,
+  providers,
 })
 export class UserRestModule extends ModuleOverride {
+  static controllers = [{ provide: USER_CONTROLLER, UserController] }];
   static dependencies = [UserModelModule];
 }
