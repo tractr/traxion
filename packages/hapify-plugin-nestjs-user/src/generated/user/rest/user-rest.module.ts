1232cb2e9f3170a328b8dd3bc252425cd86b60a1
floross <floross@users.noreply.github.com>
2021-02-26 09:23:52 -0500

    fix(nestjs-user): fix generation model inconsistence and style

--- packages/hapify-plugin-nestjs-user/src/generated/user/rest/user-rest.module.ts
+++ packages/hapify-plugin-nestjs-user/src/generated/user/rest/user-rest.module.ts
@@ -1,17 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ModuleOverride } from '@tractr/hapify-plugin-nestjs-core';
+
 import { UserModelModule } from '../common';
 import { UserController } from './controllers';
 import { UserRestDtoService } from './services';
-import { USER_CONTROLLER, USER_REST_DTO_SERVICE } from './ user-rest.constant';
+import { USER_CONTROLLER, USER_REST_DTO_SERVICE } from './user-rest.constant';
+
 const providers = [
-  { provide: USER_REST_DTO_SERVICE, useClass: UserRestDtoService }
+  {
+    provide: USER_REST_DTO_SERVICE,
+    useClass: UserRestDtoService,
+  },
 ];
+
 @Module({
   exports: providers,
   providers,
 })
 export class UserRestModule extends ModuleOverride {
-  static controllers = [{ provide: USER_CONTROLLER, UserController] }];
+  static controllers = [
+    {
+      provide: USER_CONTROLLER,
+      useClass: UserController,
+    }
+  ];
+
   static dependencies = [UserModelModule];
 }
