b2709f49a94b9639a884feab6446262a3ed80819
floross <floross@users.noreply.github.com>
2021-02-26 18:46:48 -0500

    fix: update nestjs-user and prisma version

--- packages/hapify-plugin-nestjs-user/src/generated/user/rest/controllers/user.controller.ts
+++ packages/hapify-plugin-nestjs-user/src/generated/user/rest/controllers/user.controller.ts
@@ -37,26 +37,25 @@ export class UserController {
     protected userRestDtoService: UserRestDtoService,
   ) {}
 
+  /**
+   * Create a new User
+   *
+   * @param bodyDto - Dto of the request body
+   * @returns a new User
+   */
   @Post()
   public async create(@Body() bodyDto: UserCreateBodyDto): Promise<User> {
     const formatedParams = this.userRestDtoService.formatCreateDto(bodyDto);
     return this.userService.create(formatedParams);
   }
 
-  @Get()
-  public async findMany(
-    @Query() queryDto: UserFindManyQueryDto,
-  ): Promise<User[]> {
-    const formatedParams = this.userRestDtoService.formatFindManyDto(queryDto);
-    return this.userService.findMany(formatedParams);
-  }
-
-  @Get('count')
-  public async count(@Query() queryDto: UserCountQueryDto): Promise<number> {
-    const formatedParams = this.userRestDtoService.formatCountDto(queryDto);
-    return this.userService.count(formatedParams);
-  }
-
+  /**
+   * Find zero or one User that matches the filter
+   *
+   * @param paramsDto - Dto of the request param
+   * @param queryDto - Dto of the request query
+   * @returns a User or null
+   */
   @Get(':id')
   public async findOne(
     @Param() paramsDto: UserFindUniqueParamsDto,
@@ -67,6 +66,43 @@ export class UserController {
     return this.userService.findUnique(formatedParams);
   }
 
+  /**
+   * Find zero or more User entities that matches the filter
+   *
+   * @param queryDto - Dto of the request query
+   * @returns an array of User entities
+   */
+  @Get()
+  public async findMany(
+    @Query() queryDto: UserFindManyQueryDto,
+  ): Promise<User[]> {
+    const formatedParams = this.userRestDtoService.formatFindManyDto(queryDto);
+    return this.userService.findMany(formatedParams);
+  }
+
+  /**
+   * Count the number of User entities that matches the filter
+   *
+   * @param queryDto - Dto of the request query
+   * @returns the number of User
+   */
+  @Get('count')
+  public async count(@Query() queryDto: UserCountQueryDto): Promise<number> {
+    const formatedParams = this.userRestDtoService.formatCountDto(queryDto);
+    return this.userService.count(formatedParams);
+  }
+
+  /**
+   * Update one User
+   *
+   * @Remarks
+   *
+   * Partial updates are allowed with this method
+   *
+   * @param paramsDto - Dto of the request param
+   * @param bodyDto - Dto of the request body
+   * @returns the updated User
+   */
   @Patch(':id')
   public async update(
     @Param() paramsDto: UserUpdateParamsDto,
@@ -79,6 +115,18 @@ export class UserController {
     return this.userService.update(formatedParams);
   }
 
+  /**
+   * Update or create one User
+   *
+   * @Remarks
+   *
+   * Partial updates are forbidden with this method. It will
+   * fully replace the matched entity
+   *
+   * @param paramsDto - Dto of the request param
+   * @param bodyDto - Dto of the request body
+   * @returns the updated User
+   */
   @Put(':id')
   public async upsert(
     @Param() paramsDto: UserUpsertParamsDto,
@@ -91,6 +139,12 @@ export class UserController {
     return this.userService.upsert(formatedParams);
   }
 
+  /**
+   * Delete one User
+   *
+   * @param paramsDto - Dto of the request param
+   * @returns the updated User
+   */
   @Delete(':id')
   public async delete(@Param() paramsDto: UserDeleteParamsDto): Promise<User> {
     const formatedParams = this.userRestDtoService.formatDeleteDto(paramsDto);
