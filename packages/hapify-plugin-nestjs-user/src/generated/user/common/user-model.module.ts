fe92e8646db4770bdae23b62a6722eba5aef4fe6
floross <floross@users.noreply.github.com>
2021-02-26 09:23:52 -0500

    fix(nestjs-user): fix generation model inconsistence and style

--- packages/hapify-plugin-nestjs-user/src/generated/user/common/user-model.module.ts
+++ packages/hapify-plugin-nestjs-user/src/generated/user/common/user-model.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ModuleOverride } from '@tractr/hapify-plugin-nestjs-core';
+
 import { UserDatabaseService, UserService } from './services';
 import { USER_DATABASE_SERVICE, USER_SERVICE } from './user-model.constant';
+
 const providers = [
   {
     provide: USER_SERVICE,
-    useClass: UserService
+    useClass: UserService,
   },
   {
     provide: USER_DATABASE_SERVICE,
-    useClass: UserDatabaseService],
+    useClass: UserDatabaseService,
   },
 ];
+
 @Module({
-  exports: [providers],
-  providers: [providers],
+  exports: providers,
+  providers,
 })
 export class UserModelModule extends ModuleOverride {}
