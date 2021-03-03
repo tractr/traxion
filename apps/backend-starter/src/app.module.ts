01d1870a20b7645477f4ea57c9b50841524e0993
floross <floross@users.noreply.github.com>
2021-03-03 15:00:11 -0500

    refactor(nestjs-core): rename hapify-plugin-nestjs-core to nestjs-core

--- apps/backend-starter/src/app.module.ts
+++ apps/backend-starter/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { CoreModule } from '@tractr/hapify-plugin-nestjs-core';
+import { CoreModule } from '@tractr/nestjs-core';
 import { DatabaseModule } from '@tractr/hapify-plugin-nestjs-database';
 import {
   AuthenticationModule,
