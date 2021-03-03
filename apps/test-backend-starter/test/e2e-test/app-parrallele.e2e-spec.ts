a33102a5fc29fd904aad49e6663ea4d336124a1e
floross <floross@users.noreply.github.com>
2021-03-03 10:49:37 -0500

    feat(nestjs-testing): add nestjs testing tools to use a different prisma database for each test

---
+++ apps/test-backend-starter/test/e2e-test/app-parrallele.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { createTestContext } from '@tractr/nestjs-testing';
+import request from 'supertest';
+
+import { AppModule } from '../../src/app.module';
+
+const context = createTestContext(AppModule);
+
+describe('AppController (e2e)', () => {
+  it('App should be defined', () => {
+    const { app } = context;
+    expect(app).toBeDefined();
+  });
+
+  it('App should reach the / endpoint', () => {
+    const { app } = context;
+    return request(app.getHttpServer()).get('/').expect(200).expect({});
+  });
+
+  it('App should reach the / endpoint', () => {
+    const { app } = context;
+    return request(app.getHttpServer()).get('/').expect(200).expect({});
+  });
+
+  it('App should reach the / endpoint', () => {
+    const { app } = context;
+    return request(app.getHttpServer()).get('/').expect(200).expect({});
+  });
+
+  it('App should reach the / endpoint', () => {
+    const { app } = context;
+    return request(app.getHttpServer()).get('/').expect(200).expect({});
+  });
+
+  it('App should reach the / endpoint', () => {
+    const { app } = context;
+    return request(app.getHttpServer()).get('/').expect(200).expect({});
+  });
+});
