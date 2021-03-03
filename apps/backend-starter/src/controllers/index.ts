cb0df92fe94c39f3cc871b9c3d2cf4080f7af536
floross <floross@users.noreply.github.com>
2021-03-03 14:39:50 -0500

    refactor: move test-backend-starter to backend-starter and remove documentation

--- apps/backend-starter/src/controllers/index.ts
+++ apps/backend-starter/src/controllers/index.ts
@@ -1,1 +1,2 @@
 export * from './app.controller';
+export * from './user-custom.controller';
--- apps/test-backend-starter/src/controllers/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get('')
-  public helloWorld(): string {
-    return 'Hello World! App is alive';
-  }
-}
