ff098e27dec5ebfe6d8b34aa93740fa25d3734de
floross <floross@users.noreply.github.com>
2021-03-03 10:49:37 -0500

    feat(nestjs-testing): add nestjs testing tools to use a different prisma database for each test

--- apps/test-backend-starter/src/app.module.ts
+++ apps/test-backend-starter/src/app.module.ts
@@ -6,27 +6,16 @@ import {
   AuthenticationModule,
   JwtAuthGuard,
 } from '@tractr/hapify-plugin-nestjs-user';
-import { USER_CONTROLLER } from '@tractr/hapify-plugin-nestjs-user/lib/src/generated/user';
 
 import { AppController, UserCustomController } from './controllers';
 import { ModelsModule, USER_SERVICE } from './generated';
 import { UserCustomService } from './services';
 
-const overrides = {
-  providers: [{ provide: USER_SERVICE, useClass: UserCustomService }],
-};
-
 @Module({
   imports: [
     CoreModule,
     DatabaseModule,
-    ModelsModule.register({
-      ...overrides,
-
-      controllers: [
-        { provide: USER_CONTROLLER, useClass: UserCustomController },
-      ],
-    }),
+    ModelsModule.register(),
     // AuthenticationModule.register(undefined, overrides),
   ],
   controllers: [AppController],
