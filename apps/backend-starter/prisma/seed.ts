028fa73e52a3b8ba118117be81e750d8638c778c
maxmousse <33755271+maxmousse@users.noreply.github.com>
2021-03-11 14:53:40 -0500

    refactor: split templates into several packages (#5)
    * refactor(template packages): split template into several packages
    
    * fix: update package-lock.json
    
    * fix: update seed imports and eslint config
    
    * fix: add an empty test to nestjs-authentication plugin
    
    * ci: add generated folders to github artifacts
    
    * fix: try to use postinstall npm run build on apps/* to generate prisma client
    
    * ci: dont cache the build into an github artifact and run build when needed
    
    Co-authored-by: floross <floross@users.noreply.github.com>

--- apps/backend-starter/prisma/seed.ts
+++ apps/backend-starter/prisma/seed.ts
@@ -1,24 +1,24 @@
 import { PrismaClient } from '@prisma/client';
 
-import { seedProfiles } from '../test/generated/profile/mocks';
-import { seedRights } from '../test/generated/right/mocks';
-import { seedRoles } from '../test/generated/role/mocks';
-import { seedUsers } from '../test/generated/user/mocks';
+import { seedProfiles } from '../mock/generated/profile/common';
+import { seedRights } from '../mock/generated/right/common';
+import { seedRoles } from '../mock/generated/role/common';
+import { seedUsers } from '../mock/generated/user/common';
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const rights = await seedRights(prisma, 10);
-  const roles = await seedRoles(prisma, 10, rights);
-  const users = await seedUsers(prisma, 10, roles);
-  const profiles = await seedProfiles(prisma, 10, users);
+  try {
+    const rights = await seedRights(prisma, 10);
+    const roles = await seedRoles(prisma, 10, rights);
+    const users = await seedUsers(prisma, 10, roles);
+    const profiles = await seedProfiles(prisma, 10, users);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-seed()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+seed();
---
+++ packages/nestjs-authentication/test/unit/app.spec.ts
@@ -0,0 +1,6 @@
+describe('should add tests', () => {
+  it('should add tests', () => {
+    const dummie = 'dummie';
+    expect(dummie).toBeDefined();
+  });
+});
