a80d2ab337e87091589eadd691f0a3ed5326cc19
floross <floross@users.noreply.github.com>
2021-03-29 14:40:18 -0400

    fix: add a generic parameter to have a correct return type

--- packages/nestjs-core/src/helpers/validate-env.helper.ts
+++ packages/nestjs-core/src/helpers/validate-env.helper.ts
@@ -1,14 +1,14 @@
 import { ClassConstructor, plainToClass } from 'class-transformer';
 import { validateSync } from 'class-validator';
 
-export function validateEnv(classValidator: ClassConstructor<unknown>) {
+export function validateFactory<T>(classValidator: ClassConstructor<T>) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return (config: Record<string, any>): any => {
+  return (config: Record<string, any>): T => {
     const validatedConfig = plainToClass(classValidator, config, {
       enableImplicitConversion: true,
     });
     // eslint-disable-next-line @typescript-eslint/ban-types
-    const errors = validateSync(validatedConfig as object, {
+    const errors = validateSync((validatedConfig as unknown) as object, {
       skipMissingProperties: false,
     });
 
