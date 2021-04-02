ab893733650253d0aa17cbaaab50277183855563
Florian Rossiaud <floross@users.noreply.github.com>
2021-04-02 11:49:10 -0400

    feat: put the prisma log into the nestjs logger (#51)
    * feat: put the prisma log into the nestjs logger
    
    * fix: change nestjs-core version into database module
    
    * fix: remove Prisma dependencies from database to only use global types

--- packages/nestjs-database/src/database.module.ts
+++ packages/nestjs-database/src/database.module.ts
@@ -1,10 +1,18 @@
 import { Global, Module } from '@nestjs/common';
+import { PrismaClientOptions } from '@prisma/client/runtime';
+import { LoggerModule, ModuleOptionsHelper } from '@tractr/nestjs-core';
 
+import { PRISMA_OPTIONS } from './config';
+import { PRISMA_MODULE_OPTIONS } from './constants';
 import { DatabaseService } from './services';
 
 @Global()
 @Module({
+  imports: [LoggerModule],
   providers: [DatabaseService],
   exports: [DatabaseService],
 })
-export class DatabaseModule {}
+export class DatabaseModule extends ModuleOptionsHelper<PrismaClientOptions>(
+  PRISMA_MODULE_OPTIONS,
+  PRISMA_OPTIONS,
+) {}
