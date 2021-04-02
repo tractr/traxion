89fe58e46806357ef219f4f5106a460d967ab18d
floross <floross@users.noreply.github.com>
2021-04-02 09:49:07 -0400

    feat: put the prisma log into the nestjs logger

--- packages/nestjs-database/src/services/database.service.ts
+++ packages/nestjs-database/src/services/database.service.ts
@@ -1,16 +1,44 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  Inject,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClientOptions } from '@prisma/client/runtime';
+import { Logger } from '@tractr/nestjs-core';
+
+import { PRISMA_MODULE_OPTIONS } from '../constants';
 
 @Injectable()
 export class DatabaseService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    super({ rejectOnNotFound: true });
+  constructor(
+    @Inject(PRISMA_MODULE_OPTIONS)
+    protected readonly prismaOptions: PrismaClientOptions,
+    protected readonly logger: Logger,
+  ) {
+    super(prismaOptions);
+    this.logger.setContext('DatabaseService:PrismaClient');
   }
 
   async onModuleInit(): Promise<void> {
     await this.$connect();
+
+    (this as any).$on('query', (e: Prisma.QueryEvent) =>
+      this.logger.debug(e.query),
+    );
+    (this as any).$on('warn', (e: Prisma.LogEvent) =>
+      this.logger.warn(e.message),
+    );
+    (this as any).$on('info', (e: Prisma.LogEvent) =>
+      this.logger.log(e.message),
+    );
+    (this as any).$on('error', (e: Prisma.LogEvent) =>
+      this.logger.error(e.message),
+    );
   }
 
   async onModuleDestroy(): Promise<void> {
