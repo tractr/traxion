ff8b164acea07f1f05429c146781efa2d5f002e8
Thibaut <thibaut@tractr.net>
2021-04-09 16:15:05 -0400

    fix router outlet

--- apps/angular-pwa/src/app/app-routing.module.ts
+++ apps/angular-pwa/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
