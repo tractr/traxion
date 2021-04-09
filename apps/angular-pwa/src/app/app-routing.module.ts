6299a3017469b64aaa07a902a439722bfeaff1a9
Thibaut <thibaut@tractr.net>
2021-04-09 15:56:33 -0400

    init angular 11 with ng-zorro

--- apps/angular-pwa/src/app/app-routing.module.ts
+++ apps/angular-pwa/src/app/app-routing.module.ts
@@ -9,6 +9,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
