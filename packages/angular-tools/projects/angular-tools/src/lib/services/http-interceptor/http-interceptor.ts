4ed32a46b2988d0322bad8b192b7b326015105cc
Thibaut <thibaut@tractr.net>
2021-04-16 11:36:53 -0400

    fix(angular tools): http-interceptor error

--- packages/angular-tools/projects/angular-tools/src/lib/services/http-interceptor/http-interceptor.ts
+++ packages/angular-tools/projects/angular-tools/src/lib/services/http-interceptor/http-interceptor.ts
@@ -13,10 +13,8 @@ import { CustomEncoder } from './custom-encoder';
 
 @Injectable()
 export class EncodeHttpParamsInterceptor implements HttpInterceptor {
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler,
-  ): Observable<HttpEvent<any>> {
+  intercept( req: HttpRequest<any>, next: HttpHandler,) //: Observable<HttpEvent<any>> 
+  {
     const params = new HttpParams({
       encoder: new CustomEncoder(),
       fromString: req.params.toString(),
