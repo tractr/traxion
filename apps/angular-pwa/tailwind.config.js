f59986b1d2d2814c3d10e75ea62a31dcc3a1f48d
Thibaut <thibaut@tractr.net>
2021-04-23 15:54:03 -0400

    feat(angular-pwa): add tailwind

---
+++ apps/angular-pwa/tailwind.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+      preflight: false,
+    }
+};
