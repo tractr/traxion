af0d7e744558675fca9ebdc107b643fe857f9d6b
Florian Rossiaud <floross@users.noreply.github.com>
2021-04-29 16:02:17 -0400

    feat: add angular pwa dockerfile and tweak deploy gitaction (#97)

--- apps/angular-pwa/.hapifyrc.js
+++ apps/angular-pwa/.hapifyrc.js
@@ -3,11 +3,12 @@ const {
   getValidatorPath,
 } = require('@tractr/hapify-config');
 
+// Hapify configuration
 module.exports = {
   ...hapifyDefaultConfig,
   version: '1',
-  name: 'Angular pwa starter',
-  description: 'A pwa starter to get an Angular squeleton',
+  name: 'Angular frontend starter',
+  description: 'A frontend starter to get an Angular squeleton',
   logo: 'https://miro.medium.com/max/816/1*mn6bOs7s6Qbao15PMNRyOA.png',
   validatorPath: getValidatorPath(__dirname),
   project: '../../hapify-models.json',
