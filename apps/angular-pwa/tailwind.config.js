98312047b45892db15aa63a063a2beecdaf0d9d3
floross <floross@users.noreply.github.com>
2021-05-05 16:27:57 -0400

    feat: add tailwind configuration to the angular pwa squeleton

---
+++ apps/angular-pwa/tailwind.config.js
@@ -0,0 +1,19 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+};
