671bf7277d0d689726a06ebf2cbaa567c6f35e89
Thibaut <thibaut@tractr.net>
2021-05-13 15:16:46 -0400

    feat(angular-pwa): add storybook

---
+++ apps/angular-pwa/src/app/storybook.module.ts
@@ -0,0 +1,7 @@
+import { AngularToolsModule } from "@tractr/angular-tools";
+
+import { AntCustomModule } from "./shared/ant-custom.module";
+
+export const DefaultStorybookModules = [
+	AntCustomModule, AngularToolsModule
+];
