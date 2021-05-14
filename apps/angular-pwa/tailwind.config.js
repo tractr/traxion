5010da0f0e31f5cc5a7b16d1d1f8e5be92a5549e
floross <floross@users.noreply.github.com>
2021-05-14 12:08:29 -0400

    fix: update angular pwa configuration

--- apps/angular-pwa/tailwind.config.js
+++ apps/angular-pwa/tailwind.config.js
@@ -1,19 +1,23 @@
-const { guessProductionMode } = require("@ngneat/tailwind");
+/* eslint-disable */
+const { guessProductionMode } = require('@ngneat/tailwind');
 
 module.exports = {
-    prefix: '',
-    purge: {
-      enabled: guessProductionMode(),
-      content: [
-        './src/**/*.{html,ts}',
-      ]
-    },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+  prefix: '',
+  purge: {
+    enabled: guessProductionMode(),
+    content: ['./src/**/*.{html,ts}'],
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+  ],
 };
