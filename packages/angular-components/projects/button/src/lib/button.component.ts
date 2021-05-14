7ca013e950f83f4a7458d352c9b469494682a408
Thibaut <thibaut@tractr.net>
2021-05-14 11:09:29 -0400

    feat(angular-components): add btn component

---
+++ packages/angular-components/projects/button/src/lib/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tui-button',
+  template: `
+    <p>
+      button works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ packages/angular-components/projects/button/src/lib/button.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './button.component';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ButtonComponent
+  ]
+})
+export class ButtonModule { }
