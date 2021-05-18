393ecab0192c516b2e0328846f14d80ef05ca2b7
floross <floross@users.noreply.github.com>
2021-05-18 16:48:42 -0400

    feat: start to configure storybook and chromatic

--- packages/angular-components/projects/angular-components/src/lib/angular-components.module.ts
+++ packages/angular-components/projects/angular-components/src/lib/angular-components.module.ts
@@ -1,23 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 import { AngularComponentsComponent } from './angular-components.component';
 import { ButtonComponent } from './button/button.component';
 
-import { NzButtonModule } from 'ng-zorro-antd/button';
-
-
-
 @NgModule({
-  declarations: [
-    AngularComponentsComponent,
-    ButtonComponent
-  ],
-  imports: [
-    NzButtonModule
-  ],
-  exports: [
-    AngularComponentsComponent,
-    ButtonComponent,
-    NzButtonModule
-  ]
+  declarations: [AngularComponentsComponent, ButtonComponent],
+  imports: [NzButtonModule],
+  exports: [AngularComponentsComponent, ButtonComponent, NzButtonModule],
 })
-export class AngularComponentsModule { }
+export class AngularComponentsModule {}
--- packages/angular-components/projects/angular-components/src/lib/button/stories/button.storie.ts
+++ packages/angular-components/projects/angular-components/src/lib/button/stories/button.storie.ts
@@ -1,42 +1,44 @@
-import { ButtonComponent } from '../button.component';
 import { CommonModule } from '@angular/common';
 import { moduleMetadata } from '@storybook/angular';
 import { Story } from '@storybook/angular/types-6-0';
+
 import { DefaultStorybookModules } from '../../storybook.module';
+import { ButtonComponent } from '../button.component';
 
 interface ButtonIO {
-    type: 'primary'|'dashed'|'link'|'text'|'default';
+  type: 'primary' | 'dashed' | 'link' | 'text' | 'default';
 }
 
-const baseProps = (
-	override: Partial<ButtonIO> = {}
-): ButtonIO => Object.assign({type: 'default'}, override);
+const baseProps = (override: Partial<ButtonIO> = {}): ButtonIO => ({
+  type: 'default',
+  ...override,
+});
 
 export default {
-	title: 'Tractr/Button',
-	component: ButtonComponent,
-	decorators: [
-		moduleMetadata({
-			declarations: [ButtonComponent],
-			imports: [CommonModule, ...DefaultStorybookModules],
-		}),
-	],
+  title: 'Tractr/Button',
+  component: ButtonComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [ButtonComponent],
+      imports: [CommonModule, ...DefaultStorybookModules],
+    }),
+  ],
 };
 
 const Template: Story<ButtonComponent> = (props) => ({
-    component: ButtonComponent,
-    template: `<lib-button [type]="type"></lib-button>`,
-    props
+  component: ButtonComponent,
+  template: `<lib-button [type]="type"></lib-button>`,
+  props,
 });
 
-//Default
+// Default
 export const Default = Template.bind({});
 Default.args = baseProps();
 
-//Primary
+// Primary
 export const Primary = Template.bind({});
-Primary.args = baseProps({type: 'primary'});
+Primary.args = baseProps({ type: 'primary' });
 
-//Dashed
+// Dashed
 export const Secondary = Template.bind({});
-Secondary.args = baseProps({type: 'dashed'});
+Secondary.args = baseProps({ type: 'dashed' });
