7b1e2e6965578f253eb2d8b52c1aa74ac87d0e35
floross <floross@users.noreply.github.com>
2021-05-21 11:49:49 -0400

    fix: get back to disable ivy until we go with angular 12

--- apps/angular-pwa/.storybook/main.js
+++ apps/angular-pwa/.storybook/main.js
@@ -3,12 +3,7 @@ console.log(path.join(__dirname, '@tractr/angular-components'));
 console.log(require.resolve('@tractr/angular-components'));
 
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    require.resolve('@tractr/angular-components') +
-      '/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
