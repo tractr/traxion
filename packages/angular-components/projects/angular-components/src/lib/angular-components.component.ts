9026ecccb59ce94f6bfd82d899476b727f10b67c
ThibautMTL <75043171+ThibautMTL@users.noreply.github.com>
2021-05-21 14:57:11 -0400

    feat: configure storybook into the angular pwa app (#108)
    * feat: add tailwind configuration to the angular pwa squeleton
    
    * fix: change tailwind node_modules path directory to angular.json
    
    * fix: remove unwanted package-lock.json
    
    * fix(angular-pwa): migration to yarn
    
    * fix(angular-pwa): add style ng-zorro
    
    * feat(angular-pwa): add storybook
    
    * fix(storybook): up postcss version
    
    * feat(angular-components): add btn component
    
    * feat(angular-components): create lib components with storybook
    
    * feat(angular-components): add ng-zorro style
    
    * feat(angular-components): add tailwind
    
    * fix: remove gitignore and package lock from angular packages
    
    * feat: start to configure storybook and chromatic
    
    * fix: remove chromatic credentials from package.json
    
    * fix: get back to disable ivy until we go with angular 12
    
    * fix: remove angular component tests, it's currently bloquing the CI
    
    * fix: remove exemple stories
    
    * fix: make the superlinter happy
    
    * fix: fix eslint storybook file errors
    
    Co-authored-by: floross <floross@users.noreply.github.com>

---
+++ packages/angular-components/projects/angular-components/src/lib/angular-components.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-components',
+  template: ` <p>angular-components works!</p> `,
+  styles: [],
+})
+export class AngularComponentsComponent {}
---
+++ packages/angular-components/projects/angular-components/src/lib/storybook.module.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+export const DefaultStorybookModules = [NzButtonModule];
