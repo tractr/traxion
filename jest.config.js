b9a8190e93d399d83cb400697d45dee7e33b4676
Edouard Demotes <e.demotes@gmail.com>
2021-06-09 16:41:04 -0400

    chore: add hapify-templates-rext-client (#129)

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     '<rootDir>/libs/hapify/templates/prisma',
     '<rootDir>/libs/hapify/templates/nestjs-models',
     '<rootDir>/libs/hapify/templates/rest-dtos',
+    '<rootDir>/libs/hapify/templates/rext-client',
     '<rootDir>/libs/config/prettier',
     '<rootDir>/libs/config/eslint',
     '<rootDir>/libs/config/commit',
---
+++ libs/hapify/templates/rext-client/.hapifyrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  name: 'Hapify templates for Nestjs Rext Client',
+  description: 'Generate the Rext Client to call the generated REST API',
+  templates: [
+    {
+      path: 'generated/rext-client/index.ts',
+      engine: 'hpf',
+      input: 'all',
+    },
+    {
+      path: 'generated/rext-client/rext-client.ts',
+      engine: 'hpf',
+      input: 'all',
+    },
+    {
+      path: 'generated/rext-client/helpers/index.ts',
+      engine: 'hpf',
+      input: 'all',
+    },
+    {
+      path: 'generated/rext-client/helpers/rest-ajax.ts',
+      engine: 'hpf',
+      input: 'all',
+    },
+    {
+      path: 'generated/rext-client/helpers/transform-and-validate.ts',
+      engine: 'hpf',
+      input: 'all',
+    },
+    {
+      path: 'generated/rext-client/helpers/url.ts',
+      engine: 'hpf',
+      input: 'all',
+    },
+    {
+      path: 'generated/rext-client/services/{kebab}.service.ts',
+      engine: 'hpf',
+      input: 'one',
+    },
+    {
+      path: 'generated/rext-client/services/index.ts',
+      engine: 'hpf',
+      input: 'all',
+    },
+  ],
+};
