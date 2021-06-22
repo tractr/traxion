2f513c9fa785cfc4ff0502ff849eecd13d3be460
maxim darre <maxim.darre@gmail.com>
2021-06-22 17:55:01 -0400

    wip: updating authenticatition with new generated lib

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { UserModule } from '@generated/nestjs-models';
-import { UserController } from '@generated/nestjs-models-rest';
-
 @Module({
-  imports: [UserModule],
-  controllers: [AppController, UserController],
+  imports: [],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
