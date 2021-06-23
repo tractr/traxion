554424c046deb0e7ba3a12c671c44158b83189e9
Benjamin Teste <benjamin.teste@gmail.com>
2021-06-23 11:45:08 -0400

    feat: create nestjs mailer module

---
+++ libs/nestjs/mailer/src/lib/services/mailer.service.ts
@@ -0,0 +1,37 @@
+/* eslint-disable consistent-return */
+import { Inject, Injectable } from '@nestjs/common';
+import * as mailjet from 'node-mailjet';
+
+import { MAILJET_API_VERSION } from '../configs';
+import { MAILER_MODULE_OPTIONS } from '../constants';
+import { areWeTestingWithJest } from '../helpers';
+import { MailerOptions } from '../interfaces';
+
+@Injectable()
+export class MailerService {
+  mailjetClient: mailjet.Email.Client;
+
+  constructor(
+    @Inject(MAILER_MODULE_OPTIONS)
+    private readonly mailerOptions: MailerOptions,
+  ) {
+    this.connectToMailjetApi();
+  }
+
+  public send(
+    params: mailjet.Email.SendParams,
+  ): Promise<mailjet.Email.PostResponse> | void {
+    if (!areWeTestingWithJest()) {
+      return this.mailjetClient
+        .post('send', { version: MAILJET_API_VERSION })
+        .request(params);
+    }
+  }
+
+  private connectToMailjetApi(): void {
+    this.mailjetClient = mailjet.connect(
+      this.mailerOptions.publicApiKey,
+      this.mailerOptions.privateApiKey,
+    );
+  }
+}
