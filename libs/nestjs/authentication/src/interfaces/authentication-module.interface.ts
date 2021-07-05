5285dab9feb04cbe70e3e5eaf48d003f8513ce02
Florian Rossiaud <floross@users.noreply.github.com>
2021-07-05 14:41:53 -0400

    fix: update integration test and fix the way the cookie are handled (#152)

--- libs/nestjs/authentication/src/interfaces/authentication-module.interface.ts
+++ libs/nestjs/authentication/src/interfaces/authentication-module.interface.ts
@@ -1,5 +1,6 @@
 import { JwtModuleOptions } from '@nestjs/jwt';
 import { IAuthModuleOptions } from '@nestjs/passport';
+import { CookieOptions } from 'express';
 import { StrategyOptions } from 'passport-jwt';
 import { IStrategyOptionsWithRequest } from 'passport-local';
 
@@ -12,6 +13,7 @@ export interface AuthenticationOptions {
   cookies: {
     cookieName: string;
     queryParamName: string;
+    options: CookieOptions;
   };
   strategy: {
     jwt: StrategyOptions;
