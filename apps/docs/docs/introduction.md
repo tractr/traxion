8bdbf7e4795b95a07c2b1c8eed1a6aefe1dcb92d
floross <floross@users.noreply.github.com>
2021-08-06 17:07:19 -0400

    feat: add documentation to the stack via docusaurus

---
+++ apps/docs/docs/introduction.md
@@ -0,0 +1,8 @@
+---
+id: introduction
+title: Introduction
+---
+
+Traxion is a group of node packages to install in your environment to help you
+focus on what is important in your application. It is build on top of [hapify](https://www.hapify.io/)
+a template engine for CRUD operation based on your projects' data models.
---
+++ apps/docs/sidebars.js
@@ -0,0 +1,12 @@
+module.exports = {
+  documentation: [
+    { type: 'doc', label: 'Introduction', id: 'introduction' },
+    {
+      type: 'category',
+      label: 'Overview',
+      items: ['overview/first-steps', 'overview/hapify'],
+    },
+    { type: 'category', label: 'Exemples', items: ['doc1', 'doc2', 'doc3'] },
+    { type: 'category', label: 'MDX !', items: ['mdx'] },
+  ],
+};
