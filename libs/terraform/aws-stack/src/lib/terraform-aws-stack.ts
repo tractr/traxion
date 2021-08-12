69746cfa87c552c2ee7075c9628673aa355ad6ad
Florian Rossiaud <floross@users.noreply.github.com>
2021-08-12 14:46:27 -0400

    feat: add terraform aws stack package (#233)

---
+++ libs/terraform/aws-stack/src/lib/terraform-aws-stack.ts
@@ -0,0 +1,25 @@
+import { AwsProvider } from '@cdktf/provider-aws';
+import { TerraformStack } from 'cdktf';
+import { Construct } from 'constructs';
+
+import { AwsStackConfig } from './interfaces';
+
+import { AwsProviderConstruct } from '@tractr/terraform-aws-component';
+
+export abstract class AwsStack<T extends AwsStackConfig>
+  extends TerraformStack
+  implements AwsProviderConstruct
+{
+  readonly provider: AwsProvider;
+
+  readonly name: string;
+
+  protected readonly config: T;
+
+  protected constructor(scope: Construct, id: string, config: T) {
+    super(scope, id);
+    this.config = config;
+    this.name = config.name;
+    this.provider = new AwsProvider(this, 'aws', config.awsProviderConfig);
+  }
+}
