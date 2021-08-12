98908322690843c8310a7acffd4ed8bd1adff41e
Florian Rossiaud <floross@users.noreply.github.com>
2021-08-12 14:46:27 -0400

    feat: add terraform aws stack package (#233)

---
+++ libs/terraform/aws-stack/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'terraform-aws-stack',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/terraform/aws-stack',
+};
