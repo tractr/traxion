bb02e9807298b85948e2eb2fb8027b02b1c18bdd
Florian Rossiaud <floross@users.noreply.github.com>
2021-08-26 17:36:51 -0400

    feat: add documentation to the stack via docusaurus (#252)
    * feat: add documentation to the stack via docusaurus
    
    * feat: add how to docusaurus first page

---
+++ apps/docs/sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+  documentation: [
+    { type: 'doc', label: 'Introduction', id: 'introduction' },
+    {
+      type: 'category',
+      label: 'Overview',
+      items: ['overview/first-steps', 'overview/hapify'],
+    },
+    {
+      type: 'category',
+      label: 'How to',
+      items: ['how-to/docusaurus'],
+    },
+    { type: 'category', label: 'Exemples', items: ['doc1', 'doc2', 'doc3'] },
+    { type: 'category', label: 'MDX !', items: ['mdx'] },
+  ],
+};
