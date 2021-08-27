2133f4bdb6ee709b61e01b79564a81362a5b1788
Glenn Inizan <glenn@tractr.net>
2021-08-27 13:12:44 -0500

    feat(docs): add code generation docs

---
+++ apps/docs/docs/code-generation/hapify.md
@@ -0,0 +1,23 @@
+---
+id: hapify
+title: How to configurate Hapify
+sidebar_label: Hapify
+keywords: 
+  - code-generation
+  - hapify
+tags:
+  - label: Hapify
+    permalink: hapify
+---
+
+:::info
+- Package's author: Edouard
+:::
+
+# Install
+
+`npm i --save @tractr/hapify-config`
+
+# Description
+
+TODO
--- apps/docs/sidebars.js
+++ apps/docs/sidebars.js
@@ -16,6 +16,11 @@ module.exports = {
       label: 'Commons',
       items: ['commons/common', 'commons/nestjs-core', 'commons/angular-tools'],
     },
+    {
+      type: 'category',
+      label: 'Génération de code',
+      items: ['code-generation/hapify'],
+    },
     { type: 'category', label: 'Exemples', items: ['doc1', 'doc2', 'doc3'] },
     { type: 'category', label: 'MDX !', items: ['mdx'] },
   ],
