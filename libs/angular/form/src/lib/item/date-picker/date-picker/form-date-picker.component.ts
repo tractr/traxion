2ba3d9fbd8ff809bc2dacd69558d3b0d52bba327
Thibaut <thibaut@tractr.net>
2021-11-16 15:25:05 -0500

    feat(angular-form): display time in a datepicker

--- libs/angular/form/src/lib/item/date-picker/date-picker/form-date-picker.component.ts
+++ libs/angular/form/src/lib/item/date-picker/date-picker/form-date-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 import { FormDatePickerBaseComponent } from '../form-date-picker-base.component';
@@ -9,6 +9,14 @@ import { FormDatePickerBaseComponent } from '../form-date-picker-base.component'
   styleUrls: ['./form-date-picker.component.less'],
 })
 export class FormDatePickerComponent extends FormDatePickerBaseComponent {
+  /** Customize Format Date */
+  @Input() format = 'dd-MM-yyyy';
+
+  /** Show or not time in Datepicker
+   * example nzFormat : "HH:mm:ss"
+   */
+  @Input() showTime: boolean | { nzFormat: string } = false;
+
   constructor(private formBuilder: FormBuilder) {
     super();
   }
