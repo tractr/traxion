d7c494aa612ffbe0e52646afef4e6d60827dec4c
ThibautMTL <75043171+ThibautMTL@users.noreply.github.com>
2021-11-22 11:34:50 -0500

    feat: add date and time picker into angular components (#305)
    * feat(angular-components): add input to display time in date-picker
    
    * feat(angular-form): display time in a datepicker
    
    * feat(angular-components): add time-picker component
    
    * feat(angular-form): add component time-picker
    
    Co-authored-by: Florian Rossiaud <floross@users.noreply.github.com>

--- libs/angular/form/src/lib/item/picker/date-picker/form-date-picker.component.ts
+++ libs/angular/form/src/lib/item/picker/date-picker/form-date-picker.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
-import { FormDatePickerBaseComponent } from '../form-date-picker-base.component';
+import { FormPickerBaseComponent } from '../form-picker-base.component';
 
 @Component({
   selector: 'tractr-form-date-picker',
   templateUrl: './form-date-picker.component.html',
   styleUrls: ['./form-date-picker.component.less'],
 })
-export class FormDatePickerComponent extends FormDatePickerBaseComponent {
+export class FormDatePickerComponent extends FormPickerBaseComponent {
+  /** Customize Format Date */
+  @Input() format = 'dd-MM-yyyy';
+
+  /** Show or not time in Datepicker
+   * example nzFormat : "HH:mm:ss"
+   */
+  @Input() showTime: boolean | { nzFormat: string } = false;
+
   constructor(private formBuilder: FormBuilder) {
     super();
   }
--- libs/angular/form/src/lib/item/picker/index.ts
+++ libs/angular/form/src/lib/item/picker/index.ts
@@ -1,1 +1,2 @@
 export * from './date-picker/form-date-picker.component';
+export * from './time-picker/form-time-picker.component';
