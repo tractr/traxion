ce184343ad351a34e7d8570aeee2707cb66d049a
floross <floross@users.noreply.github.com>
2021-11-29 18:02:05 -0500

    fix: move hapify user definition to angular authentication (temporary)

--- libs/angular/authentication/.hapifyrc.js
+++ libs/angular/authentication/.hapifyrc.js
@@ -10,6 +10,6 @@ module.exports = {
   description: 'A lib that will host all shared file from hapify',
   logo: 'https://miro.medium.com/max/816/1*mn6bOs7s6Qbao15PMNRyOA.png',
   validatorPath: getValidatorPath(process.cwd()),
-  project: '../../nestjs/authentication/hapify-plugin-user-model.json',
+  project: './hapify-plugin-user-model.json',
   extends: ['../../hapify/templates/models'],
 };
