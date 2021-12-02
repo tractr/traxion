576a6dabc2bb130eac8868803f13a8bdc41de15b
maxim darre <maxim.darre@gmail.com>
2021-12-02 16:43:16 -0500

    feat: add a custom class-validator decorator to make custom validation

---
+++ libs/common/src/lib/validator-transformer/constraints/custom-validator.constraint.ts
@@ -0,0 +1,35 @@
+import {
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type IsCustomConstraint<T = any> = (currentObject: T) => boolean;
+
+/**
+ * Constraint for validating that a property match a predicate passed in param.
+ */
+@ValidatorConstraint({ name: 'customConstraint', async: false })
+export class CustomConstraint implements ValidatorConstraintInterface {
+  validate(
+    propertyValue: string,
+    { constraints, object }: ValidationArguments,
+  ) {
+    const constraintsList: Array<IsCustomConstraint> = constraints;
+
+    if (constraintsList.length === 0)
+      throw new Error('No constraints specified.');
+
+    return constraintsList.every((constraint) => {
+      const result = constraint(object);
+      if (result === true) return true;
+      if (result === false) return false;
+      throw new Error('Invalid constraint.');
+    });
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `Failed custom constraint on property "${args.property}".`;
+  }
+}
