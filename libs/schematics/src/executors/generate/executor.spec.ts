9321a923773e8568405088bf139d1f1568dd749a
Killian Challeau <killian@tractr.net>
2021-12-15 15:13:39 -0500

    feat: add executor for generating hapify

--- libs/schematics/src/executors/generate/executor.spec.ts
+++ libs/schematics/src/executors/generate/executor.spec.ts
@@ -1,14 +1,29 @@
+import { execAsync } from '../../helpers/exec-async';
 import executor from './executor';
 import { GenerateExecutorSchema } from './schema';
 
+jest.mock('../../helpers/async-exec');
+
 describe('Generate executor:generate', () => {
-  it('should return the good options parameters', async () => {
+  it('executor should return the good options parameters and have called 5 commands', async () => {
+    const execAsyncSpy: jest.SpyInstance = execAsync as jest.MockedFunction<
+      typeof execAsync
+    >;
+
     const options: GenerateExecutorSchema = {
-      path: 'pathTest',
-      projectName: 'projectNameTest',
+      path: 'libs/generated',
+      projectName: '',
     };
 
     const output = await executor(options);
-    expect(output).toBe({ ...options, success: true });
+
+    expect(output).toEqual({ ...options, success: true });
+
+    expect(execAsyncSpy).toHaveBeenCalledTimes(6);
+
+    execAsyncSpy.mock.calls.forEach((call) => {
+      expect(call[0]).toEqual(expect.any(String));
+      expect(call[1]).toHaveProperty('cwd');
+    });
   });
 });
--- libs/schematics/src/executors/generate/executor.ts
+++ libs/schematics/src/executors/generate/executor.ts
@@ -1,9 +1,5 @@
-import { exec } from 'child_process';
-import { promisify } from 'util';
-
+import { execAsync } from '../../helpers/exec-async';
 import { GenerateExecutorSchema } from './schema';
-
-const execAsync = promisify(exec);
 
 export default async function runExecutor(options: GenerateExecutorSchema) {
   const { path } = options;
@@ -11,35 +7,31 @@ export default async function runExecutor(options: GenerateExecutorSchema) {
   const countParentFolder = projectName.split('/').length + 1;
   const pathToNodeModules = `${'../'.repeat(countParentFolder)}node_modules`;
 
-  await execAsync(`npx rimraf src/lib/generated`, { cwd: path }).catch((err) =>
-    console.error(err),
-  );
+  await execAsync(`npx rimraf src/lib/generated`, { cwd: path });
 
   await execAsync(
     `node ${pathToNodeModules}/@tractr/hapify-generate-config/src/index.js`,
     {
       cwd: path,
     },
-  ).catch((err) => console.error(err));
-
-  await execAsync(`npx hpf generate`, { cwd: path }).catch((err) =>
-    console.error(err),
   );
+
+  await execAsync(`npx hpf generate`, { cwd: path });
 
   await execAsync(`mv generated/${projectName} src/lib/generated`, {
     cwd: path,
-  }).catch((err) => console.error(err));
+  });
 
   await execAsync(`npx rimraf generated`, {
     cwd: path,
-  }).catch((err) => console.error(err));
+  });
 
   await execAsync(
     `node ${pathToNodeModules}/@tractr/update-templates-import-path/src/index.js`,
     {
       cwd: path,
     },
-  ).catch((err) => console.error(err));
+  );
 
   return {
     success: true,
@@ -47,21 +39,3 @@ export default async function runExecutor(options: GenerateExecutorSchema) {
     path,
   };
 }
-
-// "commands": [
-//   "npx rimraf generated",
-//   "cp -r ../../dist/libs/hapify/hapify-common ../../node_modules/@tractr",
-//   "node ../../dist/libs/hapify/generate-config/src/index.js",
-//   "npx hpf generate",
-//   "node ../../dist/libs/hapify/update-templates-import-path/src/index.js"
-// ],
-
-// "npx rimraf src/lib/generated",
-// "node ../../../node_modules/.bin/hpf-generate-config",
-// {
-//   "command": "npx hpf generate",
-//   "forwardAllArgs": false
-// },
-// "mv generated/rest-dtos src/lib/generated",
-// "npx rimraf generated",
-// "node ../../../tools/hapify-replace-path.js"
