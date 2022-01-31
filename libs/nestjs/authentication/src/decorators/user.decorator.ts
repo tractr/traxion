365c651ca15f7f05647efeecc0b1b1d6663e5b63
Maxim Darré <33755271+maxmousse@users.noreply.github.com>
2022-01-31 15:27:08 -0500

    feat: update nestjs-authentication and nestjs-casl to handle graphql (#361)

--- libs/nestjs/authentication/src/decorators/user.decorator.ts
+++ libs/nestjs/authentication/src/decorators/user.decorator.ts
@@ -1,22 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
+
+import { getRequestFromContext } from '@tractr/nestjs-core';
 
 /**
  * Get the user from the request
  */
 export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
-  },
-);
-
-/**
- * Get the user from the request in GraphQL
- */
-export const CurrentGqlUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.user;
-  },
+  (_data: unknown, context: ExecutionContext) =>
+    getRequestFromContext(context).user,
 );
--- libs/nestjs/authentication/src/guards/jwt-global-auth.guard.ts
+++ libs/nestjs/authentication/src/guards/jwt-global-auth.guard.ts
@@ -1,50 +1,28 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GUARDS_METADATA } from '@nestjs/common/constants';
 import { Reflector } from '@nestjs/core';
-import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
-import { IS_PUBLIC_KEY } from '@tractr/nestjs-core';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Injectable()
-export class JwtGlobalAuthGuard extends AuthGuard('jwt') {
-  constructor(private readonly reflector: Reflector) {
-    super();
+export class JwtGlobalAuthGuard extends JwtAuthGuard {
+  constructor(protected readonly reflector: Reflector) {
+    super(reflector);
   }
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    // Check if the route is public
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    // If we have the @Public decorator on our route we juste let pass the request
-    if (isPublic) return true;
-
     const useGuardOverriding = this.reflector.getAllAndOverride(
       GUARDS_METADATA,
       [context.getHandler(), context.getClass()],
     );
 
-    // If we have other guard in this method we get to them directly
+    // If we have other guard in the called method we get to them directly
     if (useGuardOverriding && useGuardOverriding.length > 0) return true;
 
-    // By default we use the JwtAuthGuard
+    // Check request authentication by using the JwtAuthGuard
     return super.canActivate(context);
-  }
-
-  handleRequest<User>(err: Error | undefined, user: User | undefined) {
-    // You can throw an exception based on either "info" or "err" arguments
-    if (err || !user) {
-      throw err || new UnauthorizedException();
-    }
-    return user;
   }
 }
