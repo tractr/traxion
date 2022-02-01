533411134ab7ae5beb46463e67fd36b7febb0c09
Benjamin Teste <benjamin.teste@gmail.com>
2022-02-01 09:31:46 -0500

    feat: add tests

--- libs/schematics/src/generators/prisma-library/generator.spec.ts
+++ libs/schematics/src/generators/prisma-library/generator.spec.ts
@@ -1,4 +1,6 @@
-import { Tree } from '@nrwl/devkit';
+import * as path from 'path';
+
+import { readProjectConfiguration, Tree } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 
 import generator from './generator';
@@ -23,9 +25,76 @@ describe('prisma-library generator', () => {
 
   it('should run successfully', async () => {
     await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
 
-    // console.log(appTree);
-    // const config = readProjectConfiguration(appTree, 'test');
-    // expect(config).toBeDefined();
+  it('should generate a .hapifyrc.js file', async () => {
+    await generator(appTree, {
+      ...options,
+    });
+
+    expect(appTree.exists('libs/test/.hapifyrc.js')).toBeTruthy();
+    expect(appTree.read('libs/test/.hapifyrc.js')?.toString()).toEqual(`const {
+  hapifyDefaultConfig,
+  getValidatorPath,
+} = require('@tractr/hapify-config');
+
+module.exports = {
+  ...hapifyDefaultConfig,
+  version: '1',
+  name: 'test',
+  description: 'Library to host generated codes',
+  validatorPath: getValidatorPath(__dirname),
+  project: '../../hapify-models.json',
+  extends: [
+    '@tractr/hapify-templates-prisma',],
+  importReplacements: {},
+};
+`);
+  });
+
+  it('should generate a prisma/seed.ts file', async () => {
+    await generator(appTree, {
+      ...options,
+    });
+
+    expect(appTree.exists('libs/test/prisma/seed.ts')).toBeTruthy();
+    expect(appTree.read('libs/test/prisma/seed.ts')?.toString())
+      .toEqual(`import { seed } from '../src/index';
+
+seed().catch((e) => {
+  // eslint-disable-next-line no-console
+  console.error('Failed to start the server. See the error below.');
+  // eslint-disable-next-line no-console
+  console.error(e);
+});`);
+  });
+
+  it('should generate a prisma/schemas/base.prisma file', async () => {
+    await generator(appTree, {
+      ...options,
+    });
+
+    const prismaClientOutputPath = path.join(
+      path.relative(`libs/test/prisma/schemas`, appTree.root),
+      'node_modules',
+    );
+
+    expect(appTree.exists('libs/test/prisma/schemas/base.prisma')).toBeTruthy();
+    expect(appTree.read('libs/test/prisma/schemas/base.prisma')?.toString())
+      .toEqual(`// ------------------------------------------------
+
+datasource db {
+  provider = "postgresql"
+  url      = env("TRACTR_DATABASE_URL")
+}
+
+generator client {
+  provider = "prisma-client-js"
+  output = "${prismaClientOutputPath}"
+  previewFeatures = ["filterJson"]
+}
+`);
   });
 });
