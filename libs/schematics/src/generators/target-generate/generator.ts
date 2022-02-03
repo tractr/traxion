30e262dae21881c04e945319345305582b770097
Killian Challeau <39266647+kilalo@users.noreply.github.com>
2022-02-03 16:47:01 -0500

    feat: add generator target-generate (#351)
    * feat: add generator target-generate
    
    * fix: default options
    
    * fix: default options
    
    * fix: add schematics to devdepedencies on package.json
    
    * fix: test with only required parameters

---
+++ libs/schematics/src/generators/target-generate/generator.ts
@@ -0,0 +1,93 @@
+import {
+  formatFiles,
+  names,
+  readProjectConfiguration,
+  Tree,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+import { readJSON } from 'fs-extra';
+
+import {
+  addPackageToPackageJson,
+  PackageDefinition,
+  Packages,
+} from '../../helpers';
+import { TargetGenerateGeneratorSchema } from './schema';
+
+export const SCHEMATICS_TRACTR_PACKAGE_NAME = '@tractr/schematics';
+
+export const SCHEMATICS_PACKAGE_JSON_PATH = `${__dirname}/../../../package.json`;
+
+interface NormalizedSchema extends TargetGenerateGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+}
+
+export async function normalizeOptions(
+  tree: Tree,
+  options: TargetGenerateGeneratorSchema,
+): Promise<NormalizedSchema> {
+  if (!options.project) {
+    throw new Error(`You must specify a valid project name`);
+  }
+
+  const projectName = names(options.project).fileName;
+  const projectConfiguration = readProjectConfiguration(tree, projectName);
+  const projectRoot = projectConfiguration.root;
+
+  return {
+    ...options,
+    projectName,
+    projectRoot,
+  };
+}
+
+export default async function targetGenerateGenerator(
+  tree: Tree,
+  options: TargetGenerateGeneratorSchema,
+) {
+  const normalizedOptions = await normalizeOptions(tree, options);
+
+  const { outputGeneratedPath, inputHapifyGeneratedPath, format, cleanFirst } =
+    normalizedOptions;
+
+  const baseOptions = {
+    cwd: normalizedOptions.projectRoot,
+  };
+
+  // Get the project configuration
+  const project = readProjectConfiguration(tree, normalizedOptions.projectName);
+
+  project.targets = project.targets || {};
+  project.targets.generate = {
+    executor: `${SCHEMATICS_TRACTR_PACKAGE_NAME}:generate`,
+    options: {
+      ...(outputGeneratedPath && {
+        outputGeneratedPath,
+      }),
+      ...(inputHapifyGeneratedPath && {
+        inputHapifyGeneratedPath,
+      }),
+      ...(format === false && { format: false }),
+      ...(cleanFirst === false && { cleanFirst: false }),
+      ...baseOptions,
+    },
+  };
+
+  // Update the project configuration with the release target
+  updateProjectConfiguration(tree, normalizedOptions.projectName, project);
+
+  await formatFiles(tree);
+
+  const packageJsonSchematics: PackageDefinition = await readJSON(
+    SCHEMATICS_PACKAGE_JSON_PATH,
+  );
+
+  const packages: Packages = {
+    packageName: SCHEMATICS_TRACTR_PACKAGE_NAME,
+    version: packageJsonSchematics.version,
+  };
+
+  // update the package.json dependencies
+  await addPackageToPackageJson(tree, packages);
+}
