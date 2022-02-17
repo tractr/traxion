79c700ddd019a9b2facd415d16d7ad5635ef4bf9
maxim darre <maxim.darre@gmail.com>
2022-02-17 12:55:38 -0500

    feat: add tag fetch example

--- apps/pwa/src/app/app.component.ts
+++ apps/pwa/src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { TagService } from '@tractr/generated-angular-rext-client';
+import { Tag } from '@tractr/generated-models';
 
 @Component({
   selector: 'stack-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   hello$ = this.http.get<string>('/api/hello');
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tagService: TagService) {}
+
+  public tags!: Observable<Tag[]>;
+
+  ngOnInit(): void {
+    this.tags = this.tagService.findMany$();
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   log(a: any) {
