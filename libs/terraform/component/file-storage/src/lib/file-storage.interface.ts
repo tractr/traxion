b90965bb871a3b4c22009e5a210f2d93224c9940
Edouard Demotes <e.demotes@gmail.com>
2022-04-14 15:09:06 -0400

    refactor(terraform-component-file-storage): config with instances

--- libs/terraform/component/file-storage/src/lib/file-storage.interface.ts
+++ libs/terraform/component/file-storage/src/lib/file-storage.interface.ts
@@ -1,8 +1,10 @@
+import { s3 } from '@cdktf/provider-aws';
+
 import { S3ComponentArtifacts } from '@tractr/terraform-component-s3';
 import { S3UserComponentArtifacts } from '@tractr/terraform-component-s3-user';
 
 export interface FileStorageComponentConfig {
-  additionalReadOnlyS3Arns?: string[];
+  additionalReadOnlyS3Buckets?: s3.S3Bucket[];
   s3PublicRead?: boolean;
   s3AllowUpload?: {
     origins: string[];
