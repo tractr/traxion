0317a613b0d214d2c00fe4fd637dec159d41f9c8
Killian Challeau <39266647+kilalo@users.noreply.github.com>
2022-05-30 15:38:47 -0400

    fix: use session storage only on production (#433)
    * fix: use session storage only on production
    
    * fix: add ignoreStorage props in BootstrapAppWithConfigOptions

--- libs/angular/config/src/lib/helpers/bootstrap-app-with-config.helper.ts
+++ libs/angular/config/src/lib/helpers/bootstrap-app-with-config.helper.ts
@@ -6,6 +6,7 @@ import { fetchAppConfigJson } from './fetch-app-config-json.helper';
 
 export interface BootstrapAppWithConfigOptions<T> {
   appConfigUrl?: string;
+  ignoreStorage?: boolean;
   getConfig?: (angularConfig: AngularConfig) => T;
 }
 
@@ -15,26 +16,30 @@ export async function bootstrapAppWithConfig<T>(
   AppModule: Type<unknown>,
   options?: BootstrapAppWithConfigOptions<T>,
 ) {
-  const stringifyConfiguration = sessionStorage.getItem(
-    ANGULAR_CONFIGURATION_SESSION_STORAGE,
-  );
-
   let config;
 
-  if (typeof stringifyConfiguration === 'string') {
-    try {
-      config = JSON.parse(stringifyConfiguration);
-      // eslint-disable-next-line no-empty
-    } catch {}
-  }
-
-  if (typeof config === 'undefined') {
+  if (options?.ignoreStorage) {
     config = await fetchAppConfigJson(options?.appConfigUrl);
-
-    sessionStorage.setItem(
+  } else {
+    const stringifyConfiguration = sessionStorage.getItem(
       ANGULAR_CONFIGURATION_SESSION_STORAGE,
-      JSON.stringify(config),
     );
+
+    if (typeof stringifyConfiguration === 'string') {
+      try {
+        config = JSON.parse(stringifyConfiguration);
+        // eslint-disable-next-line no-empty
+      } catch {}
+    }
+
+    if (typeof config === 'undefined') {
+      config = await fetchAppConfigJson(options?.appConfigUrl);
+
+      sessionStorage.setItem(
+        ANGULAR_CONFIGURATION_SESSION_STORAGE,
+        JSON.stringify(config),
+      );
+    }
   }
 
   config = options?.getConfig
