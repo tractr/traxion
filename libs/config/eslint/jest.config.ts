77585fae70e1d96b4762bc7636f9fdf3f0b2d0e1
Florian Rossiaud <floross@users.noreply.github.com>
2022-06-21 14:59:47 -0400

    feat: update stack's packages (#444)
    * feat: update nx to 14.1.9
    
    * fix: put dependencies in order inside the package.json
    
    * fix: update minor external packages

---
+++ libs/config/eslint/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'config-eslint',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/config/eslint',
+};
