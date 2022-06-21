0f6350ecf8b513a34a9c2048876b2f701557faba
Florian Rossiaud <floross@users.noreply.github.com>
2022-06-21 14:59:47 -0400

    feat: update stack's packages (#444)
    * feat: update nx to 14.1.9
    
    * fix: put dependencies in order inside the package.json
    
    * fix: update minor external packages

---
+++ libs/terraform/service/api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'terraform-service-api',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/terraform/service/api',
+};
