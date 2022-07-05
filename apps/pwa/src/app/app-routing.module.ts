f06ee4e7f534c73a5fd0edc8c5b8984dc5afa1f5
floross <floross@users.noreply.github.com>
2022-07-04 18:56:23 -0600

    feat: add e2e code coverage

--- apps/pwa/src/app/app-routing.module.ts
+++ apps/pwa/src/app/app-routing.module.ts
@@ -3,44 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MoleculeConnectedComponent } from './molecules/molecule-connected/molecule-connected.component';
-import { MoleculeHomeComponent } from './molecules/molecule-home/molecule-home.component';
-import { MoleculeNotConnectedComponent } from './molecules/molecule-not-connected/molecule-not-connected.component';
-
-import {
-  IsLoggedGuard,
-  IsNotLoggedGuard,
-} from '@tractr/angular-authentication';
-import { HasRoleGuard } from '@tractr/angular-casl';
-
 const routes: Routes = [
   {
-    path: 'connected',
-    component: MoleculeConnectedComponent,
-    canActivate: [IsLoggedGuard, HasRoleGuard],
-    data: {
-      redirectTo: 'redirect-after-have-not-the-role',
-      roles: { some: ['admin', 'test'] },
-    },
-  },
-  {
-    path: 'not-connected',
-    component: MoleculeNotConnectedComponent,
-    canActivate: [IsNotLoggedGuard],
-  },
-  {
-    path: 'redirect-after-have-not-the-role',
-    component: MoleculeHomeComponent,
-  },
-  {
     path: '',
-    component: MoleculeHomeComponent,
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    loadChildren: () => import('./demo/demo.module').then((m) => m.DemoModule),
   },
 ];
 
