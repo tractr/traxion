1d2c12e9123bd3e715263b6c1ab750b6fa4fed01
Edouard Demotes <e.demotes@gmail.com>
2022-10-24 15:44:19 -0400

    feat(hapify-core): add referencedIn

--- libs/hapify/core/src/lib/model.ts
+++ libs/hapify/core/src/lib/model.ts
@@ -11,10 +11,19 @@ export class Model extends Node {
   protected _fields = new Set<Field>();
 
   /**
+   * List of models which reference this model
+   */
+  protected _referencedIn = new Set<Model>();
+
+  /**
    * Adds a field to the model
    */
   addField(field: Field): this {
     this._fields.add(field);
+    // If the field is an entity field, add this model to the list of referenced models
+    if (field instanceof EntityField) {
+      field.model.addReferencedIn(this);
+    }
     return this;
   }
 
@@ -23,6 +32,10 @@ export class Model extends Node {
    */
   removeField(field: Field): this {
     this._fields.delete(field);
+    // If the field is an entity field, remove this model from the list of referenced models
+    if (field instanceof EntityField) {
+      field.model.removeReferencedIn(this);
+    }
     return this;
   }
 
@@ -51,11 +64,48 @@ export class Model extends Node {
   }
 
   /**
+   * Denotes if the model is self-dependent
+   */
+  get selfDependent(): boolean {
+    return this.fields
+      .filter((field): field is EntityField => field instanceof EntityField)
+      .some((field) => field.model === this);
+  }
+
+  /**
+   * Add a model to the list of models that reference this model
+   */
+  protected addReferencedIn(model: Model): this {
+    this._referencedIn.add(model);
+    return this;
+  }
+
+  /**
+   * Remove a model from the list of models that reference this model
+   */
+  protected removeReferencedIn(model: Model): this {
+    this._referencedIn.delete(model);
+    return this;
+  }
+
+  /**
    * The list of models that reference this model
    */
-  // get dependents(): Model[] {
-  //   return this.root.models.filter((model) =>
-  //     model.dependencies.includes(this),
-  //   );
-  // }
+  get referencedIn(): Model[] {
+    return Array.from(this._referencedIn);
+  }
+
+  /**
+   * Denotes if the model is referenced by at least one other model
+   */
+  get referenced(): boolean {
+    return this._referencedIn.size > 0;
+  }
+
+  /**
+   * Denotes if the model is referenced by itself
+   */
+  get selfReferenced(): boolean {
+    return this._referencedIn.has(this);
+  }
 }
