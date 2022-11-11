f36ec2503c559d841fc3b5bedb4d561152b1adf8
Edouard Demotes <e.demotes@gmail.com>
2022-11-11 16:37:53 -0500

    feat(hapify-core): add not operator

--- libs/hapify/core/src/lib/operators/and.ts
+++ libs/hapify/core/src/lib/operators/and.ts
@@ -1,4 +1,4 @@
-import { Predicate } from '../interfaces/predicate';
+import { Predicate } from '../interfaces';
 
 export function and<S, T1 extends S>(
   predicate1: Predicate<S, T1>,
@@ -156,18 +156,15 @@ export function and<
   predicate9?: Predicate<S, T9>,
   predicate10?: Predicate<S, T10>,
 ): Predicate<S, T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10> {
-  return (value): value is T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10 => {
-    return (
-      predicate1(value) &&
-      (!predicate2 || predicate2(value)) &&
-      (!predicate3 || predicate3(value)) &&
-      (!predicate4 || predicate4(value)) &&
-      (!predicate5 || predicate5(value)) &&
-      (!predicate6 || predicate6(value)) &&
-      (!predicate7 || predicate7(value)) &&
-      (!predicate8 || predicate8(value)) &&
-      (!predicate9 || predicate9(value)) &&
-      (!predicate10 || predicate10(value))
-    );
-  };
+  return (value): value is T1 & T2 & T3 & T4 & T5 & T6 & T7 & T8 & T9 & T10 =>
+    predicate1(value) &&
+    (!predicate2 || predicate2(value)) &&
+    (!predicate3 || predicate3(value)) &&
+    (!predicate4 || predicate4(value)) &&
+    (!predicate5 || predicate5(value)) &&
+    (!predicate6 || predicate6(value)) &&
+    (!predicate7 || predicate7(value)) &&
+    (!predicate8 || predicate8(value)) &&
+    (!predicate9 || predicate9(value)) &&
+    (!predicate10 || predicate10(value));
 }
