9c928bea58431770ea25e9c390fa57eec8e530d7
Edouard Demotes <e.demotes@gmail.com>
2023-01-16 21:46:16 -0500

    feat(hapify-core): use union types for numbers

--- libs/hapify/core/src/lib/fields/number/types/number-longitude-field.ts
+++ libs/hapify/core/src/lib/fields/number/types/number-longitude-field.ts
@@ -1,7 +1,6 @@
-import { Field } from '../field';
-import { NumberField } from './number-field';
+import { BaseNumberField } from '../base-number-field';
 
-export class NumberLongitudeField extends NumberField {
+export class NumberLongitudeField extends BaseNumberField {
   protected _min = -180;
 
   protected _max = 180;
@@ -19,11 +18,4 @@ export class NumberLongitudeField extends NumberField {
   setMax(max: number): this {
     return super.setMax(Math.min(max, 180));
   }
-}
-
-/**
- * Checks if a field is a number longitude field
- */
-export function isLongitude(field: Field): field is NumberLongitudeField {
-  return field instanceof NumberLongitudeField;
 }
