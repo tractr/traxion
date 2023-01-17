f496827f780e4725905ba7dd0804779b83e88e77
Edouard Demotes <e.demotes@gmail.com>
2023-01-16 22:13:35 -0500

    feat(hapify-core): use union types for files

---
+++ libs/hapify/core/src/lib/fields/file/types/index.ts
@@ -0,0 +1,5 @@
+export * from './file-audio-field';
+export * from './file-basic-field';
+export * from './file-image-field';
+export * from './file-video-field';
+export * from './file-document-field';
