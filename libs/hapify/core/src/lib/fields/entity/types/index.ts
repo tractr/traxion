6759c4588d5af30b9acf9a39c7ecfbd8cf962577
Edouard Demotes <e.demotes@gmail.com>
2023-01-16 22:28:59 -0500

    feat(hapify-core): use union types for entities

---
+++ libs/hapify/core/src/lib/fields/entity/types/index.ts
@@ -0,0 +1,5 @@
+export * from './entity-multiple-field';
+export * from './entity-one-to-one-field';
+export * from './entity-many-to-one-field';
+export * from './entity-one-to-many-field';
+export * from './entity-many-to-many-field';
