172799b335fe51c99a684e8a7759e7631021df7c
Edouard Demotes <e.demotes@gmail.com>
2023-01-16 22:13:35 -0500

    feat(hapify-core): use union types for files

--- libs/hapify/core/src/lib/fields/file/file-field.ts
+++ libs/hapify/core/src/lib/fields/file/file-field.ts
@@ -1,50 +1,14 @@
-import { Field } from '../field';
+import {
+  FileAudioField,
+  FileBasicField,
+  FileDocumentField,
+  FileImageField,
+  FileVideoField,
+} from './types';
 
-export class FileField extends Field {
-  /**
-   * Max size of the file
-   */
-  protected _maxSize: number | undefined;
-
-  /**
-   * Allowed extensions of the file
-   */
-  protected _allowedExtensions = new Set<string>();
-
-  /**
-   * Set max size of the file
-   */
-  setMaxSize(maxSize: number): this {
-    this._maxSize = maxSize;
-    return this;
-  }
-
-  /**
-   * Set allowed extensions of the file
-   */
-  addAllowedExtension(allowedExtension: string): this {
-    this._allowedExtensions.add(allowedExtension);
-    return this;
-  }
-
-  /**
-   * Get max size of the file
-   */
-  get maxSize(): number | undefined {
-    return this._maxSize;
-  }
-
-  /**
-   * Get allowed extensions of the file
-   */
-  get allowedExtensions(): string[] {
-    return Array.from(this._allowedExtensions);
-  }
-}
-
-/**
- * Checks if a field is a file field
- */
-export function isFile(field: Field): field is FileField {
-  return field instanceof FileField;
-}
+export type FileField =
+  | FileBasicField
+  | FileAudioField
+  | FileImageField
+  | FileVideoField
+  | FileDocumentField;
