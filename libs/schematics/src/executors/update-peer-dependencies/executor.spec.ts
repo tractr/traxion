c04b3f5fa4a2aa82c4b28ec3487e306aead14966
floross <floross@users.noreply.github.com>
2023-01-25 11:58:05 +0100

    feat: add peer dependencies executor helper to publish pacakges

---
+++ libs/schematics/src/executors/update-peer-dependencies/executor.spec.ts
@@ -0,0 +1,11 @@
+import { UpdateDependenciesExecutorSchema } from './schema';
+import executor from './executor';
+
+const options: UpdateDependenciesExecutorSchema = {};
+
+describe('UpdateDependencies Executor', () => {
+  it('can run', async () => {
+    const output = await executor(options);
+    expect(output.success).toBe(true);
+  });
+});
