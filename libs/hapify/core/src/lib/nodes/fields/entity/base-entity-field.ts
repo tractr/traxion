9c003b1b7e0818cba93e1f8209072675e3b4d218
Edouard Demotes <e.demotes@gmail.com>
2023-01-26 14:48:03 -0500

    test(hapify-core): add tests for entity fields

--- libs/hapify/core/src/lib/nodes/fields/entity/base-entity-field.ts
+++ libs/hapify/core/src/lib/nodes/fields/entity/base-entity-field.ts
@@ -1,6 +1,9 @@
 import type { Model } from '../../model';
 import { BaseField } from '../base-field';
 
+/**
+ * This class is tested via the children classes, especially the EntityOneToOneField class
+ */
 export abstract class BaseEntityField extends BaseField {
   constructor(name: string, protected _model: Model) {
     super(name);
