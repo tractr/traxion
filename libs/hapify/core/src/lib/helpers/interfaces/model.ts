919887ca97fc02ab27a6b85452fc9490c10cec62
Edouard Demotes <e.demotes@gmail.com>
2023-01-26 16:51:03 -0500

    feat(hapify-core): add dependency predicates

--- libs/hapify/core/src/lib/helpers/interfaces/model.ts
+++ libs/hapify/core/src/lib/helpers/interfaces/model.ts
@@ -4,6 +4,22 @@ export interface ModelWithOwner {
   owner: Model;
 }
 
+export interface ModelWithDependencies {
+  hasDependencies: true;
+}
+
+export interface ModelSelfDependent {
+  isSelfDependent: true;
+}
+
+export interface ModelReferenced {
+  isReferenced: true;
+}
+
+export interface ModelSelfReferenced {
+  isSelfReferenced: true;
+}
+
 export interface ModelActionScope<A extends ModelAction, S extends Scope> {
   actionsScopes: { [key in A]: S };
 }
