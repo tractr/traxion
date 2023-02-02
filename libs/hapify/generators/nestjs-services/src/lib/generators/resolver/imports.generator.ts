ece14b1e12ac052100ba7cf2bcbca5a16856877a
Mickael Martos <mickael@tractr.net>
2023-02-02 18:31:23 -0500

    add imports + directory

--- libs/hapify/generators/nestjs-services/src/lib/generators/resolver/imports.generator.ts
+++ libs/hapify/generators/nestjs-services/src/lib/generators/resolver/imports.generator.ts
@@ -6,8 +6,18 @@ export function generateImports(model: Model): ImportDeclarationStructure[] {
   return [
     {
       kind: StructureKind.ImportDeclaration,
+      moduleSpecifier: `@nestjs/common`,
+      namedImports: [{ name: `Inject` }, { name: 'Injectable' }],
+    },
+    {
+      kind: StructureKind.ImportDeclaration,
+      moduleSpecifier: `@trxn/nestjs-database`,
+      namedImports: [{ name: `DATABASE_SERVICE` }],
+    },
+    {
+      kind: StructureKind.ImportDeclaration,
       moduleSpecifier: `@prisma/client`,
-      namedImports: [{ name: `Prisma` }, { name: model.name }],
+      namedImports: [{ name: `Prisma` }, { name: 'PrismaClient' }],
     },
   ];
 }
