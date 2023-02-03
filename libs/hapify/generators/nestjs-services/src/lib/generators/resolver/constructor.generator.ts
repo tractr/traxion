836132baed550c3fc3ca9b7950c99b044983a455
Mickael Martos <mickael@tractr.net>
2023-02-03 10:00:14 -0500

    add class service + its constructor

---
+++ libs/hapify/generators/nestjs-services/src/lib/generators/resolver/constructor.generator.ts
@@ -0,0 +1,31 @@
+import {
+  ConstructorDeclarationStructure,
+  Scope,
+  StructureKind,
+} from 'ts-morph';
+
+import { camel, constant, Model, pascal } from '@trxn/hapify-core';
+
+export function generateConstructor(
+  model: Model,
+): ConstructorDeclarationStructure {
+  return {
+    kind: StructureKind.Constructor,
+    parameters: [
+      {
+        kind: StructureKind.Parameter,
+        name: `prismaClient`,
+        type: `PrismaClient`,
+        scope: Scope.Private,
+        isReadonly: true,
+        decorators: [
+          {
+            kind: StructureKind.Decorator,
+            name: 'Inject',
+            arguments: [`DATABASE_SERVICE`],
+          },
+        ],
+      },
+    ],
+  };
+}
