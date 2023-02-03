39b1644558555de762b5fa7d64a8f667b0acb39f
Mickael Martos <mickael@tractr.net>
2023-02-03 14:35:50 -0500

    add find first and find many sevices methods

--- libs/generated/nestjs-models-common/src/ts-morph-generated/services/user.service.ts
+++ libs/generated/nestjs-models-common/src/ts-morph-generated/services/user.service.ts
@@ -22,4 +22,43 @@ export class UserService {
     findUnique<T extends Prisma.UserFindUniqueArgs>(args: Prisma.SelectSubset<T, Prisma.UserFindUniqueArgs>, prisma: Prisma.UserDelegate<any> = this.prismaClient.User) {
         return prisma.findUnique<T>(args);
     }
+
+    /**
+     *        Find the first User that matches the filter.
+     *        Note, that providing 'undefined' is treated as the value not being there.
+     *        Read more here: https://pris.ly/d/null-undefined
+     *        @param {UserFindFirstArgs} args - Arguments to find a User
+     *        @example
+     *        // Get one User
+     *        const user = await this.userService.findFirst({
+     *          where: {
+     *            // ... provide filter here
+     *          }
+     *        })
+     *     
+     */
+    findFirst<T extends Prisma.UserFindFirstArgs>(args: Prisma.SelectSubset<T, Prisma.UserFindFirstArgs>, prisma: Prisma.UserDelegate<any> = this.prismaClient.User) {
+        return prisma.FindFirst<T>(args);
+    }
+
+    /**
+     *        Find zero or more Users that matches the filter.
+     *        Note, that providing 'undefined' is treated as the value not being there.
+     *        Read more here: https://pris.ly/d/null-undefined
+     *        @param {UserFindManyArgs=} args - Arguments to filter and select certain fields only.
+     *        @example
+     *        // Get all Users
+     *        const users = await this.userService.findMany()
+     *       
+     *        // Get first 10 Users
+     *        const Users = await this.UserService.findMany({ take: 10 })
+     *       
+     *        // Only select the 'id'
+     *        const userWithIdOnly = await this.UserService.findMany({ select: { id: true } })
+     *       
+     *     
+     */
+    FindMany<T extends Prisma.UserFindManyArgs>(args: Prisma.SelectSubset<T, Prisma.UserFindManyArgs>, prisma: Prisma.UserDelegate<any> = this.prismaClient.User) {
+        return prisma.FindMany<T>(args);
+    }
 }
---
+++ libs/hapify/generators/nestjs-services/src/lib/generators/resolver/find-many-method.generator.ts
@@ -0,0 +1,68 @@
+import {
+  JSDocStructure,
+  MethodDeclarationStructure,
+  OptionalKind,
+  ParameterDeclarationStructure,
+  StructureKind,
+  TypeParameterDeclarationStructure,
+} from 'ts-morph';
+
+import { camel, Model, pascal } from '@trxn/hapify-core';
+
+export const generateFindManyMethod = (model: Model,): MethodDeclarationStructure => {
+
+  const parameters: ParameterDeclarationStructure[] = [
+    {
+      name: 'args',
+      kind: StructureKind.Parameter,
+      type: `Prisma.SelectSubset<T, Prisma.${pascal(model.name)}FindManyArgs>`,
+
+    },
+    {
+      kind: StructureKind.Parameter,
+      name: 'prisma',
+      type: `Prisma.${pascal(model.name)}Delegate<any>`,
+      initializer: `this.prismaClient.${pascal(model.name)}`,
+    },
+  ];
+
+  const typeParameters: OptionalKind<TypeParameterDeclarationStructure>[] = [
+    {
+      name: 'T',
+      kind: StructureKind.TypeParameter,
+      constraint: `Prisma.${pascal(model.name)}FindManyArgs`,
+    }
+  ];
+
+  const docs: JSDocStructure[] = [
+    {
+      kind: StructureKind.JSDoc,
+      description: `
+       Find zero or more ${pascal(model.name)}s that matches the filter.
+       Note, that providing 'undefined' is treated as the value not being there.
+       Read more here: https://pris.ly/d/null-undefined
+       @param {${pascal(model.name)}FindManyArgs=} args - Arguments to filter and select certain fields only.
+       @example
+       // Get all ${pascal(model.name)}s
+       const ${camel(model.name)}s = await this.${camel(model.name)}Service.findMany()
+      
+       // Get first 10 ${pascal(model.name)}s
+       const ${pascal(model.name)}s = await this.${pascal(model.name)}Service.findMany({ take: 10 })
+      
+       // Only select the 'id'
+       const ${camel(model.name)}WithIdOnly = await this.${pascal(model.name)}Service.findMany({ select: { id: true } })
+      
+    `,
+    },
+  ];
+
+
+  return {
+    kind: StructureKind.Method,
+    name: 'FindMany',
+    typeParameters,
+    parameters,
+    statements: `return prisma.FindMany<T>(args);`,
+    docs
+  };
+};
