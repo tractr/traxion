6ee9ca168b97351edc85c6e6822d85f895ece656
Mickael Martos <mickael@tractr.net>
2023-02-03 10:00:14 -0500

    add class service + its constructor

--- libs/generated/nestjs-models-common/src/ts-morph-generated/services/user.service.ts
+++ libs/generated/nestjs-models-common/src/ts-morph-generated/services/user.service.ts
@@ -1,3 +1,9 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { DATABASE_SERVICE } from "@trxn/nestjs-database";
 import { Prisma, PrismaClient } from "@prisma/client";
+
+@Injectable
+export class UserService {
+    constructor(@Inject(DATABASE_SERVICE) private readonly prismaClient: PrismaClient) {
+    }
+}
