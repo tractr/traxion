a1cdd45412b603aaf13fff5d91bfb8ad1939662f
Mickael Martos <mickael@tractr.net>
2023-02-03 10:00:14 -0500

    add class service + its constructor

--- libs/hapify/generators/nestjs-services/src/lib/generators/resolver/resolver.generator.ts
+++ libs/hapify/generators/nestjs-services/src/lib/generators/resolver/resolver.generator.ts
@@ -1,22 +1,50 @@
-import { Project } from 'ts-morph';
+import { ClassDeclarationStructure, Project, StructureKind } from 'ts-morph';
 
 import { generateImports } from './imports.generator';
 
-import { camel, Model } from '@trxn/hapify-core';
+import { camel, Model, pascal, snake } from '@trxn/hapify-core';
+import { generateConstructor } from './constructor.generator';
+
+
+export function generateResolverClass(model: Model): ClassDeclarationStructure {
+   const className = `${pascal(model.name)}Service`;
+    const constructor = generateConstructor(model);
+
+   const methods = [
+  //   generateFindUniqueMethod(model),
+  //   generateFindManyMethod(model),
+  //   generateCreateMethod(model),
+  //   generateUpdateMethod(model),
+  //   generateDeleteMethod(model),
+  //   ...generateFieldResolvers(model),
+   ];
+
+   return {
+    kind: StructureKind.Class,
+    name: className,
+    isExported: true,
+    decorators: [
+      { name: 'Injectable' },
+    ],
+    // methods,
+    ctors: [constructor],
+  };
+}
 
 export function generateResolverSourceFile(
   project: Project,
   model: Model,
   path: string,
 ) {
-  const fileName = `${camel(model.name)}.service.ts`;
+  const fileName = `${snake(model.name)}.service.ts`;
   const filePath = `${path}/${fileName}`;
 
   const sourceFile = project.createSourceFile(filePath);
 
-  // const resolverClass = generateResolverClass(model);
+   const resolverClass = generateResolverClass(model);
   const imports = generateImports(model);
   // const method = generatePrismaSelectMethod(model);
   // sourceFile.addFunction(method);
   sourceFile.addImportDeclarations(imports);
+  sourceFile.addClass(resolverClass);
 }
