60a40a555d628a7be0ad0e1caea8bc2a2d0c9de0
Mickael Martos <mickael@tractr.net>
2023-02-09 12:35:52 -0500

    feat: add constants generator

---
+++ libs/hapify/generators/nestjs-services/src/lib/generators/service/generate-providers.ts
@@ -0,0 +1,38 @@
+import { names } from '@nrwl/devkit';
+import { Project, VariableDeclarationKind } from 'ts-morph';
+
+import { Model, pascal, snake } from '@trxn/hapify-core';
+
+export function generateProvidersSourceFile(
+  project: Project,
+  model: Model,
+  path: string,
+) {
+  const fileName = `${snake(model.name)}-model.providers.ts`;
+  const filePath = `${path}/${fileName}`;
+
+  const sourceFile = project.createSourceFile(filePath);
+  const name = `${model.name.toUpperCase()}_SERVICE`;
+  const databaseName = `${model.name.toUpperCase()}_DATABASE_SERVICE`;
+
+  sourceFile.addVariableStatement({
+    isExported: true,
+    declarationKind: VariableDeclarationKind.Const,
+    declarations: [
+      {
+        name,
+        initializer: `'${name}'`,
+      },
+    ],
+  });
+  sourceFile.addVariableStatement({
+    isExported: true,
+    declarationKind: VariableDeclarationKind.Const,
+    declarations: [
+      {
+        name: databaseName,
+        initializer: `'${databaseName}'`,
+      },
+    ],
+  });
+}
