b329b33d53b53040fdb08e418370dff878444675
Mickael Martos <mickael@tractr.net>
2023-02-14 10:53:24 -0500

    feat: generate lib angular base validator

---
+++ libs/hapify/generators/angular-base-validators/src/lib/generate.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import * as path from 'path';
+
+import { Project } from 'ts-morph';
+
+
+
+import { Project as Models } from '@trxn/hapify-core';
+
+export type NestjsServiceGeneratorConfig = {
+  outputDirectory: string;
+  tsConfigFilePath: string;
+  generatedDirectory: string;
+};
+
+export function generate(
+  project: Project,
+  dataModel: Models,
+  config: NestjsServiceGeneratorConfig,
+) {
+  const { generatedDirectory, tsConfigFilePath, outputDirectory } = config;
+  const absoluteTsConfigFilePath = path.resolve(
+    outputDirectory,
+    tsConfigFilePath,
+  );
+  const absoluteGeneratedDirectory = path.resolve(
+    outputDirectory,
+    generatedDirectory,
+  );
+
+  project.addSourceFilesFromTsConfig(absoluteTsConfigFilePath);
+
+  // Clear generation directory
+  project.getDirectory(absoluteGeneratedDirectory)?.clear();
+
+  const entityPath = `${absoluteGeneratedDirectory}/services`;
+  // Generate controllers and dtos
+  dataModel.models.forEach((model) => {
+    model.fields.forEach(field => {
+      console.log(field)
+    });
+  });
+
+  
+  // generateServiceIndexFile(project,models.map(model =. model.name),entityPath)
+  // Save project to file system
+  project.saveSync();
+}
