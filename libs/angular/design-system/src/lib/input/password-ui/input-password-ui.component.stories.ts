f2a26687d6d93de2d58c30607d4f56da223d8618
Glenn Inizan <glenn@tractr.net>
2023-02-21 09:59:06 -0800

    test(angular-design-system): improve storybook of password input

--- libs/angular/design-system/src/lib/input/password-ui/input-password-ui.component.stories.ts
+++ libs/angular/design-system/src/lib/input/password-ui/input-password-ui.component.stories.ts
@@ -1,4 +1,5 @@
 import { Meta, Story } from '@storybook/angular';
+import { userEvent, within } from '@storybook/testing-library';
 
 import { InputPasswordUiComponent } from './input-password-ui.component';
 
@@ -7,31 +8,64 @@ export default {
   component: InputPasswordUiComponent,
 } as Meta<InputPasswordUiComponent>;
 
+/**
+ * Filling input with the given string
+ * @param string
+ * @param delay
+ * @returns
+ */
+function fillingPlay(string: string, delay = 100) {
+  return async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getAllByDisplayValue('')[0];
+    await userEvent.type(input, string, {
+      delay,
+    });
+  };
+}
+
 const Template: Story<InputPasswordUiComponent> = (
   args: InputPasswordUiComponent,
 ) => ({
   props: args,
 });
 
+// Default component
 export const PasswordDefault = Template.bind({});
 PasswordDefault.args = {};
 
+// Component with label
 export const PasswordWithLabel = Template.bind({});
 PasswordWithLabel.args = {
   label: 'Input Label',
 };
 
+// Component with an empty placeholder
 export const PasswordEmptyPlaceholder = Template.bind({});
 PasswordEmptyPlaceholder.args = {
   placeholder: '',
 };
 
+// Component with a custom placeholder
 export const PasswordWithPlaceholder = Template.bind({});
 PasswordWithPlaceholder.args = {
   placeholder: 'custom placeholder',
 };
 
+// Component disabled state
 export const PasswordDisabled = Template.bind({});
 PasswordDisabled.args = {
   disabled: true,
+};
+
+// Component with filling animation
+export const PasswordFilled = Template.bind({});
+PasswordFilled.play = fillingPlay('Password Filled');
+PasswordFilled.args = {};
+
+// Component with password shown state
+export const PasswordShow = Template.bind({});
+PasswordShow.play = fillingPlay('Password Visible');
+PasswordShow.args = {
+  showPassword: true,
 };
