7dbf6e33654718e1d47b98165b67dea7d5063ac8
floross <floross@users.noreply.github.com>
2023-02-22 13:39:07 +0100

    fix: update MAILER_CLIENT providers

--- libs/nestjs/mailjet/src/mailjet.module.ts
+++ libs/nestjs/mailjet/src/mailjet.module.ts
@@ -6,11 +6,11 @@ import { MailjetClient, MailjetService } from './services';
 import { MAILER_CLIENT } from '@trxn/nestjs-mailer';
 
 @Module({
-  providers: [MailjetClient, MailjetService],
-  exports: [
+  providers: [
     MailjetClient,
     MailjetService,
     { provide: MAILER_CLIENT, useExisting: MailjetService },
   ],
+  exports: [MailjetClient, MailjetService, MAILER_CLIENT],
 })
 export class MailjetModule extends ConfigurableModuleClass {}
