ab22fba5473e855517dda5781388770a0352f842
Mickael Martos <mickael@tractr.net>
2023-02-27 08:45:04 -0500

    docs: comments on the model generation in common module

--- libs/generated/nestjs-models-common/src/generate.ts
+++ libs/generated/nestjs-models-common/src/generate.ts
@@ -14,42 +14,6 @@ import {
   NestjsServiceGeneratorConfig,
 } from '@trxn/hapify-generators-nestjs-services';
 
-// TODO: to delete after testing
-// /**
-//  * Role model
-//  */
-// const roleModel = new Model('Role')
-//   .addField(new NumberBasicField('Id').setPrimary(true).makeNotWritable())
-//   .addField(new StringBasicField('name').setMaxLength(50))
-//   .addField(new StringBasicField('description').setMaxLength(200));
-
-// /**
-//  * User model
-//  */
-// const userModel = new Model('User')
-//   .addField(new NumberBasicField('Id').setPrimary(true).makeNotWritable())
-//   .addField(new StringBasicField('FirstName').setMaxLength(50))
-//   .addField(new StringBasicField('LastName').setMaxLength(50))
-//   .addField(new StringEmailField('Email'))
-//   .addField(
-//     new StringPasswordField('Password')
-//       .setValidationRegex('/([0-9a-z]+)/')
-//       .makeNotReadable(),
-//   )
-//   .addField(
-//     new StringTextField('Description')
-//       .setNullable(true)
-//       .addMetadata('foo', 'bar'),
-//   )
-//   .addField(new BooleanField('Enabled').setDefaultValue(false))
-//   .addField(
-//     new NumberIntegerField('Credits')
-//       .setDefaultValue(10)
-//       .setMax(1000)
-//       .setNotes('Amount of credits remaining'),
-//   )
-//   .addField(new EntityOneToManyField('Role', roleModel));
-
 /**
  * Role model
  */
