e664516a4b9ee90dd0221f2bb807454280e82c84
Florian Rossiaud <floross@users.noreply.github.com>
2023-03-24 18:44:46 +0100

    fix: remove warning log and let typescript deprecation (#607)

--- libs/nestjs/database/src/database.module.ts
+++ libs/nestjs/database/src/database.module.ts
@@ -1,4 +1,4 @@
-import { Logger, LoggerService, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 import { ConfigurableModuleClass } from './database.module-definition';
 import { getDefaultPrismaClient } from './factories';
@@ -26,16 +26,8 @@ import { LoggerModule } from '@trxn/nestjs-core';
 
     {
       provide: DatabaseService,
-      useFactory: (
-        managePrismaClientService: ManagedPrismaClientsService,
-        logger: LoggerService,
-      ) => {
-        logger.warn(
-          'DatabaseService token is deprecated. Use PrismaService from @trxn/nestjs-database instead.',
-        );
-        return getDefaultPrismaClient(managePrismaClientService);
-      },
-      inject: [ManagedPrismaClientsService, Logger],
+      useFactory: getDefaultPrismaClient,
+      inject: [ManagedPrismaClientsService],
     },
   ],
   exports: [
