97ea4f61be58d50fa0f3a262858df864dbc56f7a
Mickael Martos <mickael@tractr.net>
2023-05-02 10:46:35 -0400

    feat: @trxn/hapify-generators-casl-config and hapify-prisma-casl-generator

---
+++ libs/hapify/generators/casl-config/README.md
@@ -0,0 +1,12 @@
+# hapify-generators-casl-config
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build hapify-generators-casl-config` to build the library.
+
+## Running unit tests
+
+Run `nx test hapify-generators-casl-config` to execute the unit tests via
+[Jest](https://jestjs.io).
---
+++ libs/hapify/prisma/casl-generator/src/generator.ts
@@ -0,0 +1,78 @@
+import { generatorHandler } from '@prisma/generator-helper';
+import { logger } from '@prisma/internals';
+import { join } from 'path';
+import { Project } from 'ts-morph';
+
+import { version } from '../package.json';
+
+import { createSchema, Schema } from '@trxn/hapify-core';
+import { convertDmmfToHapifySchemaDeclaration } from '@trxn/hapify-devkit';
+import { hapifyCaslConfigGenerator } from '@trxn/hapify-generators-casl-config';
+
+export const GENERATOR_NAME = 'Hapify Prisma NestJs/AuthorizedServices';
+
+generatorHandler({
+  onManifest() {
+    return {
+      version,
+      prettyName: GENERATOR_NAME,
+    };
+  },
+  onGenerate: async (options) => {
+
+    const { generator, dmmf } = options;
+
+    const output = generator.output?.value;
+    const { tsConfigFilePath } = generator.config;
+
+    // Validate the generator configuration
+    if (!output) {
+      const error = `${GENERATOR_NAME}: No output directory specified in generator block`;
+      logger.error(error);
+      throw new Error(error);
+    }
+
+    if (!tsConfigFilePath) {
+      const error = `${GENERATOR_NAME}: No tsConfigFilePath specified in generator block`;
+      logger.warn(error);
+      throw new Error(error);
+    }
+
+    // Instantiate the ts project
+    const project = new Project({
+      tsConfigFilePath: join(output, tsConfigFilePath),
+    });
+
+    await project.getDirectory(output)?.clearImmediately();
+
+    try {
+      logger.log(`Convert DMMF to Hapify schema declaration`);
+      const schema: Schema = createSchema(
+        convertDmmfToHapifySchemaDeclaration(dmmf),
+      );
+
+      // hapifyNestjsAuthorizedServicesGenerator(project, schema, {
+      //   output,
+      // });
+
+      // TODO: call the generator generator-casl
+
+      console.warn(`🚜 ~ file: generator.ts:68 ~ TODO:`);
+      hapifyCaslConfigGenerator(project, schema, {
+        output,
+        });
+
+    } catch (error) {
+      logger.error(error);
+      throw error;
+    }
+
+    // Remove unused imports
+    // project
+    //   .getSourceFiles()
+    //   .map((sourceFile) => sourceFile.fixUnusedIdentifiers());
+
+    // Save project to file system
+    project.saveSync();
+  },
+});
