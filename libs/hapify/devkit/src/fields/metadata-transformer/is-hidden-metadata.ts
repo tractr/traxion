343dec1689d13fb007f4aaa60fc5b1eb4a73b63a
floross <floross@users.noreply.github.com>
2023-05-19 12:41:31 +0200

    fix: update the hidden metadata condition

--- libs/hapify/devkit/src/fields/metadata-transformer/is-hidden-metadata.ts
+++ libs/hapify/devkit/src/fields/metadata-transformer/is-hidden-metadata.ts
@@ -3,7 +3,7 @@ import { FieldDeclaration } from '@trxn/hapify-core';
 export function hiddenMetadata(field: FieldDeclaration): FieldDeclaration {
   const { hidden, ...metadata } = field.metadata || {};
 
-  if (typeof hidden === 'boolean' || hidden === 'true' || hidden === 'false') {
+  if ((typeof hidden === 'boolean' && hidden) || hidden === 'true') {
     return {
       ...field,
       isHidden: true,
